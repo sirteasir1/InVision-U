7f5fbcc34d97b20c30759daae301a550f1a80e74
Yerbolat <rgalbeke@gmail.com>
2026-04-05 18:42:18 +0500

    Delete app directory

--- app/candidate/layout.tsx
+++
@@ -1,4 +0,0 @@
-import { FormProvider } from '@/context/form-context'
-export default function CandidateLayout({ children }: { children: React.ReactNode }) {
-  return <FormProvider>{children}</FormProvider>
-}
--- app/committee/page.tsx
+++
@@ -1,4 +0,0 @@
-import { redirect } from 'next/navigation'
-export default function CommitteePage() {
-  redirect('/committee/candidates')
-}
