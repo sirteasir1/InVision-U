704880c819485451462998687b82d95bc7d7e7eb
Yerbolat <rgalbeke@gmail.com>
2026-04-05 17:11:24 +0500

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from 'next/server'
-
-export function isCommitteeAuthorized(req: NextRequest): boolean {
-  const pw = req.headers.get('x-committee-password')
-  return pw === process.env.COMMITTEE_PASSWORD
-}
